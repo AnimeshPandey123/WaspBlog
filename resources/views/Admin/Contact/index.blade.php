661a7abc82c733b08a334dfb771140959e7386c8
AnimeshPandey123 <ani.mesh13578@gmail.com>
2018-03-21 13:14:21 +0545

    Message show

--- resources/views/Admin/Contact/index.blade.php
+++ resources/views/Admin/Contact/index.blade.php
@@ -60,9 +60,7 @@
  			<th>
  				Message
  			</th>
- 			<th>
- 				Show
- 			</th>
+ 			
  			<th>
  				Trash
  			</th>
@@ -74,9 +72,7 @@
  				<tr>
  				<td>{{$message->name}}</td>
  				<td>{{$message->description}}</td>
- 				<td>
-						<button class="" id="myBtn"> Show </button>
- 				</td>
+ 				
  				<td>
  				<a href="{{route('message.delete',['id'=>$message->id])}}" class="btn btn-xs btn-danger" > Trash </a>
  			</td>
@@ -91,7 +87,7 @@
  						</th>
  					
  				</tr>
- 				@endif
+ 			@endif
  			</tbody>
 
 
@@ -100,48 +96,14 @@
 
  			</div>
  			
-  <!-- The Modal -->
-<div id="myModal" class="modal">
+ 
 
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <p>Some text in the Modal..</p>
-  </div>
-
-</div>
- 		
 @stop
 @section('scripts')
-  <script type="text/javascript">
-    // Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  console.log('new');
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-       
-
+<script type="text/javascript">
     
-</script>
+    document.getElementById("myBtn").addEventListener("click", function(){
+    alert("Hello World!");
+});
+    </script>
 @stop
